f0614461c65062a90b9e160f3330b22163f8fd1a
riankarya <riankaryamarpaung@gmail.com>
2020-10-09 23:16:47 +0700

    setting for heroku

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const Task = require('./routes/route')
 const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
